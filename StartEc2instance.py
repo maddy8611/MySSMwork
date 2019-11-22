29b64497ab9821075e4b86e0f32d83eee8bf772c
maddy8611 <42824938+maddy8611@users.noreply.github.com>
2019-11-21 23:29:04 -0500

    Create StartEc2instance.py

---
+++ StartEc2instance.py
@@ -0,0 +1,49 @@
+import json
+import boto3
+import logging
+
+#setup simple logging for INFO
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+#define the connection
+ec2 = boto3.resource('ec2')
+
+def lambda_handler(event, context):
+    # Use the filter() method of the instances collection to retrieve
+    # all running EC2 instances.
+    filters = [{
+            'Name': 'tag:madhav',
+            'Values': ['YES','Yes','yes']
+        },
+        {
+            'Name': 'instance-state-name', 
+            'Values': ['stopped']
+        }
+    ]
+    
+    #filter the instances
+    instances = ec2.instances.filter(Filters=filters)
+
+    #locate all running instances
+    StoppedInstances = [instance.id for instance in instances]
+    
+    #print the instances for logging purposes
+    #print StoppedInstances 
+    
+    #make sure there are actually instances to down bring them up. 
+    if len(StoppedInstances) > 0:
+        #perform the shutdown
+        StartingInstances = ec2.instances.filter(InstanceIds=StoppedInstances).start()
+        print StartingInstances
+        return {
+            'statusCode': 200,
+            'body': json.dumps(str(StartingInstances))
+        }
+    else:
+        print "Nothing to see here"
+        return {
+            'statusCode': 200,
+            'body': json.dumps("Nothing to see here")
+        }
+
